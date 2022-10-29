a2b7e760b56022c0b774128c48f87feaaa9bccd9
kktzd <3227804189@qq.com>
2022-10-29 12:37:09 +0800

    循环语句

---
+++ 循环语句/循环语句/循环语句.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{	
+	int a = 0;
+	while (a < 30000)
+	{
+		printf("Ñ§Ï°%d\n", a);
+		a++;
+	}
+	if (a == 30000)
+	{
+		printf("³öÉ½");
+	}
+	return 0;
+}
+	
