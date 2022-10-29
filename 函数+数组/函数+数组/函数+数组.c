5f18cec437707be67c6157e453b6baee8a2c927b
kktzd <3227804189@qq.com>
2022-10-29 02:06:31 +0000

    数组+函数 c语言的数组+函数
    Signed-off-by: kktzd <3227804189@qq.com>

---
+++ 函数+数组/函数+数组/函数+数组.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//º¯Êý
+int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+	
+int main()
+//{
+//	int sum1 = 0;
+//	int sum2 = 0;
+//	scanf("%d%d", &sum1, &sum2);
+//
+//	//int sum3 = sum1 + sum2;
+//	//printf("%d\n", sum3);
+//
+//	int sum = Add(sum1, sum2);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+//Êý×é
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+	char ch[5] = { 'a','b','c' };
+}
